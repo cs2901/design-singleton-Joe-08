528b2eb5077f0e1e49ec9a6c14c46d98d409ddde
Joe-08 <joe_andre0710@hotmail.com>
2019-05-09 13:00:22 -0500

    a

---
+++ MultithreadingSingletonChocolateBoiler.java
@@ -0,0 +1,20 @@
+class Multithreading extends Thread {
+    public void run() {
+        try {
+            System.out.println("Thread " + Thread.currentThread().getId() + "is running");
+        }
+        catch (Exception e){
+            System.out.println("Exception is caught");
+        }
+    }
+}
+
+public class Multithread {
+    public static void main(String []args) {
+        int n = 8;
+        for (int i = 0; i < n; i++) {
+            Multithreading object = new Multithreading();
+            object.start();
+        }
+    }
+}
